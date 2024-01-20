fe41907186330ae716808c3392574a5350b7d13f
ABDUS SALAM NOYON <noyonahamed461@gmail.com>
2024-01-20 12:17:52 +0600

    Switch Button all done

---
+++ app/src/main/java/com/example/switchbutton/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.switchbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch aSwitch;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      aSwitch=findViewById(R.id.switchid);
+
+      aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+          @Override
+          public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+              if (isChecked){
+                  Toast.makeText(MainActivity.this, "ON", Toast.LENGTH_SHORT).show();
+              }else {
+                  Toast.makeText(MainActivity.this, "OFF", Toast.LENGTH_SHORT).show();
+              }
+          }
+      });
+
+    }
+}
